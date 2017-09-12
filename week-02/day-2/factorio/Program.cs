b4c9a43cc4ca84b2eaa53b458e946d23f68e6d9f
Katona Dorottya <moodytea@yahoo.com>
2017-09-12 13:31:44 +0200

    factorio

---
+++ week-02/day-2/factorio/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace factorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// - Create a function called `factorio`
+			//   that returns it's input's factorial
+			Console.WriteLine("Enter a number!");
+			int number = int.Parse(Console.ReadLine());
+
+			int Factorio(int n)
+			{
+				for (int i = 1; i < number; i++)
+					n = n * i;
+				return n;
+
+
+			}
+			Console.WriteLine(Factorio(number));
+			Console.ReadLine();
+        }
+    }
+}
