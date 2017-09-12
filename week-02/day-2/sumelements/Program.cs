c2dab40dae62d12c6beb3a0afcfe70e4e0c47eb3
Katona Dorottya <moodytea@yahoo.com>
2017-09-12 17:15:50 +0200

    sumelements

---
+++ week-02/day-2/sumelements/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace sumelements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// - Create an array variable named `r`
+			//   with the following content: `[54, 23, 66, 12]`
+			// - Print the sum of the second and the third element
+			
+            int[] r = { 54, 23, 66, 12 };
+            Console.WriteLine(r[2]+r[3]);
+            Console.ReadLine();
+
+        }
+    }
+}
