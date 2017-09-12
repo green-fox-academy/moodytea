423db3b8907ca7258126be5dd6acf70d22b576f5
Katona Dorottya <moodytea@yahoo.com>
2017-09-12 17:10:47 +0200

    compare lenght

---
+++ week-02/day-2/comparelenght/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace comparelenght
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// - Create an array variable named `p1`
+			//   with the following content: `[1, 2, 3]`
+			// - Create an array variable named `p2`
+			//   with the following content: `[4, 5]`
+			// - Print if `p2` has more elements than `p1`
+			Console.ReadLine();
+
+            int[] p1 = { 1, 2, 3 };
+            int[] p2 = { 4, 5 };
+
+            if (p1.Length > p2.Length){
+                Console.WriteLine("p1 is bigger");
+            }
+			else if (p2.Length > p1.Length)
+			{
+				Console.WriteLine("p2 is bigger");
+			}
+        }
+    }
+}
