a0e2eddff8ce3049af70b7fd214449d0846a184a
Katona Dorottya <moodytea@yahoo.com>
2017-09-12 16:52:56 +0200

    print arguments part1, third

---
+++ week-02/day-2/printargument/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace printarguments
+{
+	class Program
+
+	{
+        static void Printer(){
+			string input = (Console.ReadLine());
+
+			if (!input.Equals(""))
+			{
+				Console.WriteLine(input);
+			}
+
+		}
+
+		static void Main(string[] args)
+		{
+            // - Create a function called `printer`
+            //   which prints the input String parameters
+            // - It can have any number of parameters
+
+            // Examples
+            // printer("first")
+            // printer("first", "second")
+            // printer("first", "second", "third", "fourh")
+            // ...
+            Console.WriteLine("Enter strings!");
+            Printer();
+			Console.ReadLine();
+		}
+	}
+}
---
+++ week-02/day-2/third/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// - Create an array variable named `q`
+			//   with the following content: `[4, 5, 6, 7]`
+			// - Print the third element of `q`
+			Console.ReadLine();
+
+            int [] myArray = {4, 5, 6, 7};
+            Console.WriteLine(myArray[2]);
+
+        } 
+    }
+}
