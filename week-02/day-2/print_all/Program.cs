156ec9c44e6b8cb5e7241dd31bda1b08a5f978d8
Katona Dorottya <moodytea@yahoo.com>
2017-09-12 17:42:50 +0200

    print_all

---
+++ week-02/day-2/print_all/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace print_all
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `af`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print all the elements of `af`
+            int[] af = { 4, 5, 6, 7 };
+            //int first = af[1];
+            //int last = af[af.Length]; 
+            //Console.WriteLine(first>last);
+            foreach (int element in af)
+                Console.WriteLine(element);
+
+            Console.ReadLine();
+        }
+    }
+}
