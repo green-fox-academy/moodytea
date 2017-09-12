2c8cd676fe87b72b97b722ae2532afbf228e1625
Katona Dorottya <moodytea@yahoo.com>
2017-09-12 17:28:44 +0200

    incrementelement

---
+++ week-02/day-2/incrementelement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace incrementelement
+{
+    class Program
+    {
+        static void Main(string[] args)
+		{
+            //- Create an array variable named `t`
+            //  with the following content: `[1, 2, 3, 4, 5]`
+            //- Increment the third element
+            //- Print the third element
+            int[] t = {1, 2, 3, 4, 5};
+                t[4] += 1;
+            Console.WriteLine(t[4]);
+            Console.ReadLine();
+		}
+    }
+}
