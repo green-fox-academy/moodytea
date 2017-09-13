f2c75b2ed64c0786cea07631ac9936fb8e63e943
Katona Dorottya <moodytea@yahoo.com>
2017-09-13 17:21:29 +0200

    matchmaking works

--- week-02/day-3/lists/matchmaking/Program.cs
+++ week-02/day-3/lists/matchmaking/Program.cs
@@ -1,19 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace matchmaking
+namespace Matchmaking
 {
-    class Program
+    public class Matchmaking
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-			var girls = new List<string> { "Eve", "Ashley", "Bözsi", "Kat", "Jane" };
-			var boys = new List<string> { "Joe", "Fred", "Béla", "Todd", "Neef", "Jeff" };
-			var order = new List<string>();
+            var girls = new List<string> { "Eve", "Ashley", "Bözsi", "Kat", "Jane" };
+            var boys = new List<string> { "Joe", "Fred", "Béla", "Todd", "Neef", "Jeff" };
+            var order = new List<string>();
 
-			// Join the two lists by matching one girl with one boy in the order list
-			// Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+            int sizeOfList = girls.Count;
+		
+            for (int i = 0; i < sizeOfList; i++)
+            {
+                Console.WriteLine(string.Concat(girls[i]," ", boys[i]));
+            }
 
-			Console.WriteLine(order);
+            int Diff = boys.Count - girls.Count;
+
+
+            if (Diff>0){
+                Console.WriteLine(boys[boys.Count-Diff]);
+            }
+			if (Diff<0)
+			{
+                Console.WriteLine(girls[sizeOfList - Diff]);
+			}
+
+			
+        
+        // Join the two lists by matching one girl with one boy in the order list
+        // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+        Console.WriteLine();
         }
     }
 }
