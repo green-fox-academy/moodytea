a8d420c645c66dcded6c9aeccc6c67895bcd199f
Katona Dorottya <moodytea@yahoo.com>
2017-09-13 21:04:16 +0200

    elementfinder two ways done

--- week-02/day-3/lists/elementfinder/Program.cs
+++ week-02/day-3/lists/elementfinder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace elementfinder
 {
@@ -8,8 +9,26 @@ namespace elementfinder
         {
 			var List = new List<int> { 1, 2, 3, 4, 5 };
 
-			// Check if the arrayList contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
-			// Do this again with a different solution using different list functions!
+            // Check if the arrayList contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+            // Do this again with a different solution using different list functions!
+
+            //myFirstSolution:
+            //if (List.Contains(7)){
+            //Console.WriteLine("Hoorray");
+            //}
+            // else
+            //Console.WriteLine("Noooooo");
+
+            //and the second:
+            foreach (int element in List){
+                if (element==7){
+                    Console.WriteLine("Hoorray!");
+                    }
+                }
+
+            Console.WriteLine("If you didn't get a hurray, the given no. is NOT in the list. Sorry :(");
+            
+
 		}
     }
 }
