9a1bac8dc9ec873579eba1bb55ea75cd2388a4e2
Katona Dorottya <moodytea@yahoo.com>
2017-09-13 12:46:32 +0200

    todoprint

--- week-02/day-3/strings/todoprint/Program.cs
+++ week-02/day-3/strings/todoprint/Program.cs
@@ -7,16 +7,18 @@ namespace todoprint
         static void Main(string[] args)
         {
 			string todoText = " - Buy milk\n";
-			// Add "My todo:" to the beginning of the todoText
-			// Add " - Download games" to the end of the todoText
-			// Add " - Diablo" to the end of the todoText but with indention
+            // Add "My todo:" to the beginning of the todoText
+            // Add " - Download games" to the end of the todoText
+            // Add " - Diablo" to the end of the todoText but with indention
 
-			// Expected output:
+            // Expected output:
 
-			// My todo:
-			//  - Buy milk
-			//  - Download games
-			//      - Diablo
+            // My todo:
+            //  - Buy milk
+            //  - Download games
+            //      - Diablo
+
+            todoText = todoText.Insert(todoText.Length, " - Download games \n     - Diablo");
 
 			Console.WriteLine(todoText);
         }
