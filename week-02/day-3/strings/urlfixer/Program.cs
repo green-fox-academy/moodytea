bf472ab48183c2a24e0ded638697a4f577643bbd
Katona Dorottya <moodytea@yahoo.com>
2017-09-13 12:12:29 +0200

    ex2

--- week-02/day-3/strings/urlfixer/Program.cs
+++ week-02/day-3/strings/urlfixer/Program.cs
@@ -11,6 +11,9 @@ namespace urlfixer
 			// Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
 			// Also, the URL is missing a crutial component, find out what it is and insert it too!
 			// Try to solve it more than once using different string functions!
+            url = url.Replace( "bots","odds");
+            url += "/";
+            url = url.Insert(5, ":");
 
 			Console.WriteLine(url);
         }
