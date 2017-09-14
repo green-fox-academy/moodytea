715e76686d60d9dfa6b646c0276131d2e469b6e8
Katona Dorottya <moodytea@yahoo.com>
2017-09-14 11:16:53 +0200

    averageofinputs

---
+++ week-02/day-4/averageofinput/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace averageofinput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// Write a program that asks for 5 integers in a row,
+			// then it should print the sum and the average of these numbers like:
+			//
+			// Sum: 22, Average: 4.4
+			Console.WriteLine("number?");
+			int n1 = Convert.ToInt16(Console.ReadLine());
+			Console.WriteLine("number?");
+			int n2 = Convert.ToInt16(Console.ReadLine());
+			Console.WriteLine("number?");
+			int n3 = Convert.ToInt16(Console.ReadLine());
+			Console.WriteLine("number?");
+			int n4 = Convert.ToInt16(Console.ReadLine());
+			Console.WriteLine("number?");
+			int n5 = Convert.ToInt16(Console.ReadLine());
+
+            int sum = n1 + n2 + n3 + n4 + n5;
+            int av = sum / 5;
+            Console.WriteLine("sum:" + sum + " average:" + av);
+            Console.ReadLine();
+        }
+    }
+}
