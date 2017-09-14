19ec8f500a08440c9b46826ad2a42f19b09b8743
Katona Dorottya <moodytea@yahoo.com>
2017-09-14 15:46:10 +0200

    pyramid of stars first step

---
+++ week-02/day-4/pyramid/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// Write a program that reads a number from the standard input, then draws a
+			// pyramid like this:
+			//
+			//
+			//    *
+			//   ***
+			//  *****
+			// *******
+			//
+			// The pyramid should have as many lines as the number was
+
+			Console.WriteLine("Enter a number!");
+			int num = int.Parse(Console.ReadLine());
+
+			int i, j, k;
+			for (i = 1; i <= num; i++)
+			{
+				for (j = 1; j <= num - i; j++)
+				{
+					Console.Write(" ");
+				}
+				for (k = 1; k <= i; k++)
+				{
+					Console.Write("*");
+				}
+				Console.WriteLine("");
+			}
+
+            Console.Read();
+        }
+    }
+}
