2b785ca3d0fa6d5bb68f412d4806b6c64219aab9
Katona Dorottya <moodytea@yahoo.com>
2017-09-14 14:15:19 +0200

    trianglefromstars

---
+++ week-02/day-4/drawtriangle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace drawtriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+		{   // Write a program that reads a number from the standard input, then draws a
+			// triangle like this:
+			//
+			// *
+			// **
+			// ***
+			// ****
+			//
+			// The triangle should have as many lines as the number was
+
+
+
+            Console.WriteLine("Enter a number!");
+            int num = int.Parse(Console.ReadLine());
+
+			int i, j;
+            for (i = 1; i <= num; i++)
+			{
+                for (j = 1; j <= i; j++)
+				{
+					Console.Write("*");
+				}
+				Console.WriteLine("");
+			}
+			
+            Console.ReadLine();
+        }
+    }
+}
