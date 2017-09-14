2ed7c67d67c5f54d741fba49f0d8337bec48f9f9
Katona Dorottya <moodytea@yahoo.com>
2017-09-14 13:06:16 +0200

    print bigger

---
+++ week-02/day-4/printbigger/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace printbigger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// Write a program that asks for two numbers and prints the bigger one
+
+			Console.WriteLine("Two numbers!");
+			double a = int.Parse(Console.ReadLine());
+			double b = int.Parse(Console.ReadLine());
+			double c = a - b;
+
+
+            double Comparer = c > 0 ? a : b;
+            if (c==0){
+                Console.WriteLine("Warning! Equal!");
+                return;
+            }
+            Console.WriteLine(Comparer);
+              
+			
+
+
+			Console.ReadLine();
+        }
+    }
+}
