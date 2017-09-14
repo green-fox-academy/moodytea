5da071cbe5287d320ca75c27a7e49edff2bfa217
Katona Dorottya <moodytea@yahoo.com>
2017-09-14 10:40:45 +0200

    day 4 dir, bmi done

---
+++ week-02/day-4/bmi/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace bmi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// Print the Body mass index (BMI) based on these values
+			double massInKg = 72.0;
+			double heightInM = 1.75;
+            double BMI = massInKg / (heightInM *= heightInM);
+
+
+            Console.WriteLine(BMI);
+            Console.ReadLine();
+        }
+    }
+}
