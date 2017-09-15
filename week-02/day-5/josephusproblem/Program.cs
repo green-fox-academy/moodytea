7349d63b2d9bf097939673b8e961596d151d7aa7
Katona Dorottya <moodytea@yahoo.com>
2017-09-15 14:45:17 +0200

    list of numbers to josephus problem

---
+++ week-02/day-5/josephusproblem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace josephusproblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+			Console.WriteLine("Enter the number!");
+			int numberStart = int.Parse(Console.ReadLine());
+
+            var numList = new List<int>();
+            int number = 0;
+
+            while (number <= numberStart)
+			{
+                numList.Add(number);
+                number++;
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
