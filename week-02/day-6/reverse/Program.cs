16244aa4adec1bbd29e9d872a75cd3f1ff4bdad1
Katona Dorottya <moodytea@yahoo.com>
2017-09-16 19:42:41 +0200

    array elements reversed and printed

---
+++ week-02/day-6/reverse/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Reverse
+{
+	class Program
+	{
+		static void PrintArray(int[] arr)
+		{
+			for (int i = 0; i < arr.Length; i++)
+			{
+				Console.Write("Element({0})={1} \n", i, arr[i]);
+			}
+			Console.WriteLine();
+		}
+
+		static void Main(string[] args)
+		{
+            // - Create an array variable named `aj`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Reverse the order of the elements in `aj`
+            // - Print the elements of the reversed `aj`
+            int[] aj = new int[] { 3, 4, 5, 6, 7 };
+            int counter = 1;
+            var reversed = new int[aj.Length];
+            for (int i = 0; i < aj.Length; i++)
+			{
+                reversed[i] = aj[aj.Length - counter];
+				counter++;
+			}
+            PrintArray(reversed);
+			Console.ReadLine();
+		}
+	}
+}
