02e935692a444ed78a747493317a7a04323ab6af
Katona Dorottya <moodytea@yahoo.com>
2017-09-16 11:51:13 +0200

    square simple

---
+++ week-02/day-6/square1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace square1
+{
+    class Program
+    {
+        static void Main(string[] args)
+		{
+			Console.WriteLine("Enter a number!");
+			int n = int.Parse(Console.ReadLine());
+
+            int i;
+            string S = "";
+            for (i = 1; i <= n; i++ ){
+                S = S + "%";
+            }
+
+            Console.WriteLine(S);
+
+            S = "%";
+
+            for (i = 2; i <= n - 1; i++)
+            { S += " "; }
+
+            S += "%";
+
+            for (i = 2; i <= n - 1; i++){
+                Console.WriteLine(S);
+            }
+
+            S = "";
+
+			for (i = 1; i <= n; i++)
+			{
+				S = S + "%";
+			}
+            Console.WriteLine(S);
+
+
+            Console.ReadLine();
+        }
+    }
+}
