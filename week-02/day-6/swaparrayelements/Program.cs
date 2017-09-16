0a5663288e733b452f51e3da8988ddd10b8a69c8
Katona Dorottya <moodytea@yahoo.com>
2017-09-16 18:49:57 +0200

    swaparrayelements with print done

---
+++ week-02/day-6/swaparrayelements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SwapElements
+{
+	class Program
+	{
+		static void PrintArray(string[] arr)
+		{
+			for (int i = 0; i < arr.Length; i++)
+			{
+				System.Console.Write(arr[i] + "{0}", i < arr.Length - 1 ? " " : "");
+			}
+			System.Console.WriteLine();
+		}
+		static void Main(string[] args)
+		{
+			// - Create an array variable named `abc`
+			//   with the following content: `["first", "second", "third"]`
+			// - Swap the first and the third element of `abc`
+			string[] abc = new string[] { "first", "second", "third" };
+			string temp = abc[0];
+			abc[0] = abc[2];
+			abc[2] = temp;
+
+			PrintArray(abc);
+
+			Console.ReadLine();
+
+		}
+	}
+}
