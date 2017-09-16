0fca3186b84b2323f3634421c5fcad7ac50db09b
Katona Dorottya <moodytea@yahoo.com>
2017-09-16 12:21:42 +0200

    square two

---
+++ week-02/day-6/sqare_diagonally/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace square1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter a number!");
+			int n = int.Parse(Console.ReadLine());
+
+			int i, j;
+
+            for (i = 1; i <= n; i++){
+                for (j = 1; j <= n; j++){
+                    if ((i == 1) || (i == n ) || (j == 1) || (j == n))
+                        Console.Write("%");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+
+			Console.ReadLine();
+		}
+	}
+}
