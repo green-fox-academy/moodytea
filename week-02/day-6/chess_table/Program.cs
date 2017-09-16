bca452c1ac885379517436e230fa525f1573d053
Katona Dorottya <moodytea@yahoo.com>
2017-09-16 12:30:11 +0200

    chesstable

---
+++ week-02/day-6/chess_table/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace chess_table
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter a number!");
+			int n = int.Parse(Console.ReadLine());
+
+			int i, j;
+
+			for (i = 1; i <= n; i++)
+			{
+				for (j = 1; j <= n; j++)
+				{
+					if ((i + j) %2 ==0)
+						Console.Write("%");
+					else
+						Console.Write(" ");
+				}
+				Console.WriteLine();
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
