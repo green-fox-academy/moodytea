7610a691ba645b154de9d305a9132f5b4ca600ba
Katona Dorottya <moodytea@yahoo.com>
2017-09-17 10:15:07 +0200

    appendletter part two

---
+++ week-02/day-6/appendletter2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppendLetter
+{
+	public class AppendLetter
+	{
+		public static void Main(string[] args)
+		{
+
+			var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+			// Add "a" to every string in the far list.
+			for (int i = 0; i < far.Count; i++)
+			{
+                far[i] = far[i] + "a";
+			}
+
+            far.ForEach(Console.WriteLine);
+
+		}
+	}
+}
