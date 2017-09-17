52d59f42619a6b5661ee7e2997ab9b742a78875f
Katona Dorottya <moodytea@yahoo.com>
2017-09-17 18:43:04 +0200

    josephus problem is done

--- week-02/day-5/josephusproblem/Program.cs
+++ week-02/day-5/josephusproblem/Program.cs
@@ -13,6 +13,7 @@ namespace josephusproblem
 
             var numList = new List<int>();
             int number = 1;
+            bool lastDead = true;
 
             while (number <= numberStart)
 			{
@@ -24,20 +25,51 @@ namespace josephusproblem
 
             while (length > 1)
             {
-                if (length % 2 == 0)
+                if (length % 2 == 0 && lastDead)
                 {
-                    int i = ;
-                    numList.RemoveAt(i + 1);
+                    lastDead = true;
+                    for (int i = length-1; i >= 1; i-=2)
+                    {
+                        numList.RemoveAt(i);
+                    }
+
                 }
-                else if (length % 2 != 0)
-                {
-                    int i = 0;
-                    numList.RemoveAt(i + 1);
-                }
+				else if (length % 2 == 1 && lastDead)
+				{
+                    lastDead = false;
+					for (int i = length - 2; i >= 1; i -= 2)
+					{
+						numList.RemoveAt(i);
+					}
+
+				}
+
+				else if (length % 2 == 0 && !lastDead)
+				{
+                    lastDead = false;
+					for (int i = length - 2; i >= 0; i -= 2)
+					{
+						numList.RemoveAt(i);
+					}
+
+				}
+
+				else if (length % 2 == 1 && !lastDead)
+				{
+                    lastDead = true;
+					for (int i = length - 1; i >= 0; i -= 2)
+					{
+						numList.RemoveAt(i);
+					}
+
+				}
+            length = numList.Count;
+            numList.ForEach(Console.Write);
+            Console.WriteLine();
             }
 
 
-
+            numList.ForEach(Console.WriteLine);
             Console.ReadLine();
         }
     }
