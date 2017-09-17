90bffa9e42f0576d4fba073396eaed26d5d0cc38
Katona Dorottya <moodytea@yahoo.com>
2017-09-17 15:12:59 +0200

    palindrome

---
+++ week-02/day-6/palindrome/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace palindrome
+{
+    class Program
+    {
+        public static string Reversed (string s){
+			int counter = s.Length;
+            string t = "";
+
+			for (int i = counter - 1; i >= 0; i--)
+			{
+                t = t+s[i];
+			}
+            return t;
+
+        }
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Give me a word: ");
+            string str = Convert.ToString(Console.ReadLine());
+            str = (str + Reversed(str));
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ week-02/day-6/searchpalindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace searchpalindrome
+{
+    class Program
+        public static void Reversed (string reversed) 
+
+            int counter = reversed.Length;
+
+            for (int i = counter-1; i >=0; i-- ){
+                Console.Write(Convert.ToString(reversed[i]));
+            }
+        public static void SearchPalindrom(string input, List<string> output)
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+			//Create a function named search palindrome following your current language's style guide. 
+			//It should take a string, search for palindromes that at least 3 characters long and 
+			//return a list with the found palindromes.
+
+			Console.WriteLine("Give me a word or sentence to check: ");
+			string userInput = Console.ReadLine();
+			var List = new List<string> { };
+
+			SearchPalindrome(userInput, List);
+
+			Console.ReadLine();
+        }
+    }
+}
