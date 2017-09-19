56260044771f0d2f92b505f0565a0c0c0fc11c9b
moodytea <moodytea@yahoo.com>
2017-09-19 11:57:03 +0200

    divide by zero error code done

---
+++ week-02/day-1/favno/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ex07
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your fav number!");
+            String favNum = Console.ReadLine();
+            Console.WriteLine("So your fav num is: " + favNum + ". Mine too!");
+        }
+    }
+}
---
+++ week-03/day-2/DivideByZero/DivideByZero/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a function that takes a number
+            // divides ten with it,
+            // and prints the result.
+            // it should print "fail" if the parameter is 0
+            try  // Prevents the program breaking when attempting dividing by zero
+            {
+                int divisor = int.Parse(Console.ReadLine());
+                // If the input value for divisor was 0 the program breaks
+                int result = 12 / divisor;
+                // The program doesn't reach this line if the input was 0
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException e)
+            {
+                // This line only runs if the input was 0
+                Console.WriteLine("Can't divide by zero!");
+            }
+            catch (Exception e)
+            {
+                // This line only runs if uncatched exception generated
+                Console.WriteLine("uuups, some serious thing happened!");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
