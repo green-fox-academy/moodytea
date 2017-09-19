7fa8517040b92ac16f0d5a70905f827c67fa8ed7
moodytea <moodytea@yahoo.com>
2017-09-19 17:09:25 +0200

    print each line

--- week-03/day-2/PrintEachLine/PrintEachLine/PrintEachLine.cs
+++ week-03/day-2/PrintEachLine/PrintEachLine/PrintEachLine.cs
@@ -1,4 +1,24 @@
-// Write a program that opens a file called "my-file.txt", then prints
-// each of lines form the file.
-// If the program is unable to read the file (for example it does not exists),
-// then it should print an error message like: "Unable to read file: my-file.txt"
+using System;
+using System.IO;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "my-file.txt";
+            try
+            {
+                string[] content = File.ReadAllLines(path);
+                Console.WriteLine(content[0]);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Uh-oh, could not read the file!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
