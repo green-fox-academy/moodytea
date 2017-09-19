4130f451f00f83da44fadb138a39be17a77a4b41
moodytea <moodytea@yahoo.com>
2017-09-19 20:37:10 +0200

    writeSingleLine created

---
+++ week-03/day-2/WriteSingleLine/WriteSingleLine/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"./new-text.txt";
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    writer.WriteLine("My name is Dorothy Katona.");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Couldn't write file!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
