4a69ce31e1f8959d0c10b74a438cd22ff4297a9e
moodytea <moodytea@yahoo.com>
2017-09-19 17:44:08 +0200

    countlines created

---
+++ week-03/day-2/CountLines/CountLines/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Countline
+{
+    class Program
+    {
+        public static int CountLines (string path)
+        {
+            if (path == "")
+                return 0;
+            else
+                return path.Length;
+        }
+        public static void Main(string[] args)
+        {
+            String path = @"./assets/lorem-ipsum.txt";
+            Console.WriteLine(CountLines(path));
+            Console.ReadLine();
+        }
+    }
+}
