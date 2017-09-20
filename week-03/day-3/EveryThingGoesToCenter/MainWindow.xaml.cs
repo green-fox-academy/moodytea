0a93c065b797d47b8d7af47075490ca1cd5c784e
moodytea <moodytea@yahoo.com>
2017-09-21 01:15:42 +0200

    everything to center progress bit

--- week-03/day-3/EveryThingGoesToCenter/MainWindow.xaml.cs
+++ week-03/day-3/EveryThingGoesToCenter/MainWindow.xaml.cs
@@ -12,17 +12,63 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GreenFox;
 
 namespace EveryThingGoesToCenter
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
+
     {
+        public static void DrawPolygon(FoxDraw foxDraw, int x, int y)
+        {
+
+            int half = 150;
+            //int rei = 0;
+            //int full = 300;
+            var startPoint = new Point(x, y);
+            var endPoint = new Point(half, half);
+            foxDraw.DrawLine(startPoint, endPoint);
+            foxDraw.StrokeColor(Colors.Magenta);
+            foxDraw.BackgroundColor(Colors.Black);
+        }
         public MainWindow()
         {
             InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            int i, j, n;
+            int x = 0;
+            int y = 0;
+            n = 8;
+            for (i = 1; i <= n; i++)
+            {
+
+
+
+                for (j = 1; j <= n; j++)
+                {
+
+
+                    if ((i + j) % 2 == 0)
+                    {
+                        foxDraw.FillColor(Colors.Black);
+
+                    }
+
+                    else if ((i + j) % 2 != 0)
+                    {
+                        foxDraw.FillColor(Colors.Red);
+
+                    }
+                    x += 5;
+                    DrawPolygon(foxDraw, x, y);
+
+                }
+                y += 5;
+                DrawPolygon(foxDraw, x, y);
+                
+            }
+
         }
     }
 }
