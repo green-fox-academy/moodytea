2354b6310c30735dd64ba2173df57366d5e908a0
moodytea <moodytea@yahoo.com>
2017-09-20 13:05:49 +0200

    lines done

--- week-03/day-3/MiddleLines/MiddleLines/MainWindow.xaml.cs
+++ week-03/day-3/MiddleLines/MiddleLines/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Media;
 using GreenFox;
 
 namespace MiddleLines
@@ -9,10 +10,32 @@ namespace MiddleLines
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public static void DrawLines(FoxDraw foxDraw)
+        {
+            foxDraw.StrokeColor(Colors.Red);
+            var startPoint = new Point(0, 150);
+            var endPoint = new Point(300, 150);
+            foxDraw.DrawLine(startPoint, endPoint);
+        }
+        public static void DrawLines2(FoxDraw foxDraw)
+        {
+            foxDraw.StrokeColor(Colors.Green);
+            var startPoint = new Point(150, 0);
+            var endPoint = new Point(150,300);
+            foxDraw.DrawLine(startPoint, endPoint);
+        }
         public MainWindow()
         {
-            var foxDraw = new FoxDraw(canvas);
+
             InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawLines(foxDraw);
+            DrawLines2(foxDraw);
+
+            //  draw a red horizontal line to the canvas' middle.
+            //  draw a green vertical line to the canvas' middle.
+
         }
     }
 }
