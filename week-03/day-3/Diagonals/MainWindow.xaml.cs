29dcab1ef7380136c2ee1d18c2046c5790e02116
moodytea <moodytea@yahoo.com>
2017-09-20 14:08:45 +0200

    diagonals done

--- week-03/day-3/Diagonals/MainWindow.xaml.cs
+++ week-03/day-3/Diagonals/MainWindow.xaml.cs
@@ -12,17 +12,33 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GreenFox;
 
 namespace Diagonals
 {
     public partial class MainWindow : Window
     {
+        public static void DrawLines(FoxDraw foxDraw)
+        {
+
+            //int half = 150;
+            int rei = 0;
+            int full = 300;
+            foxDraw.StrokeColor(Colors.Green);
+            var startPoint = new Point(rei, rei);
+            var endPoint = new Point(full, full);
+            foxDraw.DrawLine(startPoint, endPoint);
+            startPoint = new Point(rei, full);
+            endPoint = new Point(full, rei);
+            foxDraw.DrawLine(startPoint, endPoint);
+            
+        }
         public MainWindow()
         {
             InitializeComponent();
             var foxDraw = new FoxDraw(canvas);
             // draw the canvas' diagonals in green.
-
+            DrawLines(foxDraw);
         }
     }
 }
