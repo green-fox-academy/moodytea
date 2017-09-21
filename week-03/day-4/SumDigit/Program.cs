11dbe9d415d2f058d0e82699f7184f13b7ded16e
moodytea <moodytea@yahoo.com>
2017-09-21 13:25:45 +0200

    sum digits of a number is done

--- week-03/day-4/SumDigit/Program.cs
+++ week-03/day-4/SumDigit/Program.cs
@@ -1,24 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace CountingUp
+namespace SumDigit
 {
     class Program
     {
-        public static void countUp(int num)
+        public static int countUp(int num)
         {
-            if (num > 1)
+            if (num != 0)
             {
-                countUp(num - 1);
+                return(num % 10 + countUp(num / 10));
             }
-            Console.WriteLine(num - 1);
+            else
+            {
+                return 0;
+            }
         }
         static void Main(string[] args)
         {
             // Write a recursive function that takes one parameter: n and adds numbers from 1 to n..
 
-            int n = 40;
-            countUp(n+1);
+            int n = 895138;
+            Console.WriteLine(countUp(n)); 
             Console.Read();
         }
     }
