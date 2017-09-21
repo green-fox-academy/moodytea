197fefc5877fa3cf387d37fae0d475dc43835044
moodytea <moodytea@yahoo.com>
2017-09-21 12:00:47 +0200

    counting up done

---
+++ week-03/day-4/CountingUp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountingUp
+{
+    class Program
+    {
+        public static void countUp(int num)
+        {
+            if (num > 1)
+            {
+                countUp(num - 1);
+            }
+            Console.WriteLine(num - 1);
+        }
+        static void Main(string[] args)
+        {
+            // Write a recursive function that takes one parameter: n and counts down from n.
+
+            int n = 40;
+            countUp(n+1);
+            Console.Read();
+        }
+    }
+}
