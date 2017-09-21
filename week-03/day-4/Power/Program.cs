162802546934e466bc29e351d388cb55a93ad54e
moodytea <moodytea@yahoo.com>
2017-09-21 13:45:57 +0200

    power done

---
+++ week-03/day-4/Power/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Power
+{
+    class Program
+    {
+        static int power(int baseof, int powerRaised)
+        {
+            if (powerRaised != 0)
+                return (baseof * power(baseof, powerRaised - 1));
+            else
+                return 1;
+        }
+        static void Main(string[] args)
+        {
+            int n = 3;
+            int n2 = 4;
+            Console.WriteLine(power(n, n2));
+
+            Console.ReadLine();
+        }
+    }
+}
