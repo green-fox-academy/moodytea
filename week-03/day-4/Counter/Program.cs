232146f847e54b78da6f694fefcbdc58b06200c5
moodytea <moodytea@yahoo.com>
2017-09-21 11:10:34 +0200

    counting down somewhat done

---
+++ week-03/day-4/Counter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Counter
+{
+    class Program
+    {
+        public static void Recurse(int remaining)
+        {
+            while (remaining != 0)
+            {
+                remaining -= 1;
+                Console.WriteLine(remaining);
+                
+            }
+
+            Recurse(remaining);
+
+        }
+        static void Main(string[] args)
+        {
+            // Write a recursive function that takes one parameter: n and counts down from n.
+
+            int n = 40;
+            Recurse(n + 1);
+        }
+    }
+}
