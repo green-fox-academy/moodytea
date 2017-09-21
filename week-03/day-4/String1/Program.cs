6e1a77fa7c74c43e720d49b17a89937a4c2a6be2
moodytea <moodytea@yahoo.com>
2017-09-21 17:56:55 +0200

    string 1 without recursion

---
+++ week-03/day-4/String1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace String1
+{
+    class Program
+    {
+        static string ChangeXToY (string CarriedForSearch)
+        {
+            if (CarriedForSearch != String.Empty && CarriedForSearch.Contains("x"))
+            {
+                return CarriedForSearch.Replace("x", "y");
+            }
+            else return CarriedForSearch;
+        }
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively (no loops) a new string where all the
+            // lowercase 'x' chars have been changed to 'y' chars.
+            string TextForSearch = "Xtreme extra x-factor with your x.";
+           
+            Console.WriteLine(ChangeXToY(TextForSearch)); 
+            Console.ReadKey();
+        }
+    }
+}
