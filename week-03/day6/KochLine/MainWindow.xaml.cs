eebfc57d6e5f3bc976f11cb17640a26fe8421bf3
moodytea <moodytea@yahoo.com>
2017-09-23 21:12:40 +0200

    koch upside down done

--- week-03/day6/KochLine/MainWindow.xaml.cs
+++ week-03/day6/KochLine/MainWindow.xaml.cs
@@ -25,6 +25,26 @@ namespace KochLine
         {
             InitializeComponent();
             var foxDraw = new FoxDraw(canvas);
+            Koch(foxDraw, canvas, 0, 150, 300, 3, 0);
+            foxDraw.BackgroundColor(Colors.LightGray);
+        }
+        public static void Koch(FoxDraw foxDraw, Canvas canvas, double x, double y, double length, int depth, double a)
+        {
+            double d = length / 3;
+            double pi = Math.PI;
+
+            if (depth > 0)
+            {
+                Koch(foxDraw, canvas, x, y, d, depth - 1, a);
+                Koch(foxDraw, canvas, x + d * Math.Cos(a), y + d * Math.Sin(a), d, depth - 1, a + pi / 3);
+                Koch(foxDraw, canvas, x + d * Math.Cos(a) + d * Math.Cos(a + pi/3), y + d * Math.Sin(a) + d * Math.Sin(a + pi/3), d, depth - 1, a - pi /3);
+                Koch(foxDraw, canvas, x + 2 * d * Math.Cos(a), y + 2 * d * Math.Sin(a), d, depth - 1, a);
+            }
+            else
+            {
+                foxDraw.DrawLine(x, y, x + length * Math.Cos(a), y + length * Math.Sin(a));
+            }
         }
     }
+        
 }
