553e58484eebc57d429d9c6b91dace5abface66a
moodytea <moodytea@yahoo.com>
2017-09-23 20:04:37 +0200

    envelope star also done the long way

--- week-03/day6/LinePlay/MainWindow.xaml.cs
+++ week-03/day6/LinePlay/MainWindow.xaml.cs
@@ -27,20 +27,58 @@ namespace LinePlay
             var foxDraw = new FoxDraw(canvas);
             DrawGreenLines(foxDraw);
             DrawPurpleLines(foxDraw);
+            DrawLines(foxDraw);
+            DrawLines2(foxDraw);
             //foxDraw.BackgroundColor(Colors.Black);
+        }
+        public static void DrawLines(FoxDraw foxDraw)
+        {
+            foxDraw.StrokeColor(Colors.Green);
+            int x, y, z, a;
+            x = 150;
+            y = 0;
+            z = 150;
+            a = 150;
+            for (int i = 0; i < 14; i++)
+            {
+                y += 10;
+                z -= 10;
+                var startPoint = new Point(x, y);
+                var endPoint = new Point(z, a);
+                foxDraw.DrawLine(startPoint, endPoint);
+            }
+
+        }
+        public static void DrawLines2(FoxDraw foxDraw)
+        {
+            foxDraw.StrokeColor(Colors.Green);
+            int x, y, z, a;
+            x = 150;
+            y = 300;
+            z = 150;
+            a = 150;
+            for (int i = 0; i < 14; i++)
+            {
+                y -= 10;
+                z += 10;
+                var startPoint = new Point(x, y);
+                var endPoint = new Point(z, a);
+                foxDraw.DrawLine(startPoint, endPoint);
+            }
+
         }
         public static void DrawGreenLines(FoxDraw foxDraw)
         {
             foxDraw.StrokeColor(Colors.Green);
             int x, y, z, a;
-            x = 0;
-            y = 10;
-            z = 10;
-            a = 300;
-            for (int i = 0; i < 13; i++)
+            x = 150;
+            y = 0;
+            z = 150;
+            a = 150;
+            for (int i = 0; i < 14; i++)
             {
-                y += 20;
-                z += 20;
+                y += 10;
+                z += 10;
                 var startPoint = new Point(x, y);
                 var endPoint = new Point(z, a);
                 foxDraw.DrawLine(startPoint, endPoint);
@@ -48,20 +86,21 @@ namespace LinePlay
         }
         public static void DrawPurpleLines(FoxDraw foxDraw)
         {
-            foxDraw.StrokeColor(Colors.Purple);
+            foxDraw.StrokeColor(Colors.Green);
             int x, y, z, a;
-            x = 10;
-            y = 0;
-            z = 300;
-            a = 10;
-            for (int i = 0; i < 13; i++)
+            x = 0;
+            y = 150;
+            z = 150;
+            a = 150;
+            for (int i = 0; i < 14; i++)
             {
-                x += 20;
-                a += 20;
+                x += 10;
+                a += 10;
                 var startPoint = new Point(x, y);
                 var endPoint = new Point(z, a);
                 foxDraw.DrawLine(startPoint, endPoint);
             }
+        
         }
     }
 }
