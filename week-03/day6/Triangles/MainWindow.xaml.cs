a5151c81e36bee8af060cb6aa3e390e75e3bb1c0
moodytea <moodytea@yahoo.com>
2017-09-24 22:07:33 +0200

    didn't finish triangles yet

--- week-03/day6/Triangles/MainWindow.xaml.cs
+++ week-03/day6/Triangles/MainWindow.xaml.cs
@@ -24,7 +24,39 @@ namespace Triangles
         public MainWindow()
         {
             InitializeComponent();
-            var foxDraw = new FoxDraw(canvas); 
+            var foxDraw = new FoxDraw(canvas);
+            //foxDraw.BackgroundColor(Colors.Black);
+            double x = 150;
+            double y = 0;
+            double triangleSide = 20;
+            double triangleheight = Math.Sqrt(3 * triangleSide) / 2;
+            double z = x - triangleSide/2;
+            double a = y + triangleheight;
+            DrawGreenPolygon(foxDraw, z,a, triangleSide, triangleheight);
+            DrawGreenPolygon2(foxDraw, x, y, triangleSide, triangleheight);
+            
+        }
+        public static void DrawGreenPolygon(FoxDraw foxDraw, double z, double a, double triangleSide, double triangleheight)
+        {
+            var points = new List<Point>();
+            points.Add(new Point(z, a));
+            points.Add(new Point(z + triangleSide, a));
+            points.Add(new Point(z - triangleSide/2, a + triangleheight));
+            
+
+            foxDraw.FillColor(Colors.Gray);
+            foxDraw.DrawPolygon(points);
+        }
+        public static void DrawGreenPolygon2(FoxDraw foxDraw, double x, double y, double triangleSide, double triangleheight)
+        {
+            var points = new List<Point>();
+            points.Add(new Point(x, y));
+            points.Add(new Point(x + triangleSide / 2, y + triangleheight));
+            points.Add(new Point(x - triangleSide, y));
+
+
+            foxDraw.FillColor(Colors.Gray);
+            foxDraw.DrawPolygon(points);
         }
     }
 }
