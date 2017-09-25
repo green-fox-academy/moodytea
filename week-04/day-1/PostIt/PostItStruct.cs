44cd72f0fcc0cfd66e182906df8dba3431c55b8d
Katona Dorottya <moodytea@yahoo.com>
2017-09-25 13:43:31 +0200

    postIt

---
+++ week-04/day-1/PostIt/PostItStruct.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PostIt
+{
+    public struct PostIt
+    {
+        public string BackgroundColor;
+        public string Text;
+        public string TextColor;
+    }
+
+}
