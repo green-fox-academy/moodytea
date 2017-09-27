324abe0bec878e33ca3db6aeb5c0dee3e9338d8a
Katona Dorottya <moodytea@yahoo.com>
2017-09-27 17:13:18 +0200

    anagram done

---
+++ week-04/day-3/anagram/AreTheyAnagram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace anagram
+{
+    public class AreTheyAnagram
+    {
+        public static bool SoAreTheyAnagram(string s1, string s2)
+		{
+			char[] a1 = s1.ToLower().ToArray();
+			Array.Sort(a1);
+            s1 = new string(a1);
+
+			char[] a2 = s2.ToLower().ToArray();
+			Array.Sort(a2);
+			s2 = new string(a2);
+
+            if (!s1.Equals(s2))
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+        }
+    }
+}
