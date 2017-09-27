0631168769f030c85ce444e894bbb201c9beb9c9
Katona Dorottya <moodytea@yahoo.com>
2017-09-27 22:43:21 +0200

    countLetters done

---
+++ week-04/day-3/countLetters/CountLetters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace countLetters
+{
+    public class CountLetters
+    {
+        public static Dictionary<char, int> CountTheLetters(string input)
+        {
+            Dictionary<char, int> myDict = new Dictionary<char, int>();
+
+            input = input.ToLower();
+            char[] AtArrayPhraseNow = input.ToCharArray();
+
+            foreach (char letter in AtArrayPhraseNow){
+                if (!myDict.ContainsKey(letter))
+                {
+                    myDict.Add(letter,0);
+                }
+                myDict[letter]++;
+            }
+            return myDict;
+			
+        }
+    }
+}
---
+++ week-04/day-3/countLetters/Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+
+namespace countLetters
+{
+   [TestFixture]
+   public class Tests
+   {
+        string SomeDrWhoQuote = "1'm being extremely clever up here, but there is no1 to stand around looking impressed. What'5 the p0int 1n havin9 y0u 4ll?"; 
+            
+	   [Test]
+	   public void LetterCountIntheBeggining()
+	   {
+            int counted = 3;
+            Dictionary<char, int> output = CountLetters.CountTheLetters(SomeDrWhoQuote);
+
+            Assert.AreEqual(counted, output['1']);
+	   }
+
+	   [Test]
+	   public void LetterCountIntheEnd()
+	   {
+			int counted = 5;
+			Dictionary<char, int> output = CountLetters.CountTheLetters(SomeDrWhoQuote);
+
+			Assert.AreEqual(counted, output['l']);
+	   }
+
+       [Test]
+       public void CountSpecial()
+       {
+			int counted = 1;
+			Dictionary<char, int> output = CountLetters.CountTheLetters(SomeDrWhoQuote);
+			Assert.AreEqual(counted, output['?']);
+       }
+
+       [Test]
+       public void CountSpaces()
+       {
+			int counted = 21;
+			Dictionary<char, int> output = CountLetters.CountTheLetters(SomeDrWhoQuote);
+			Assert.AreEqual(counted, output[' ']);
+       }
+   }
+}
