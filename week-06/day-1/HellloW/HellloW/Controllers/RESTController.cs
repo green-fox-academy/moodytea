ab554a60fe01dfa82b32a0a66e4fcbdf55b250d3
moodytea <moodytea@yahoo.com>
2017-10-24 14:47:46 +0200

    hello project name, id counter done

---
+++ week-06/day-1/HellloW/HellloW/Controllers/RESTController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HellloW.Models;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HellloW.Controllers
+{
+    [Route("api")]
+    public class RESTController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("greeting")]
+        public IActionResult Greating2 (string name)
+        {
+            Greating.id += 1;
+            return new JsonResult(new Greating(name));
+        }
+
+    }
+}
---
+++ week-06/day-1/HellloW/HellloW/Models/Greating.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HellloW.Models
+{
+    public class Greating
+    {
+        public long ID;
+        public static long id;
+        public string content;
+
+        public Greating(string name)
+        {
+            content = "Hello " + name;
+            this.ID = id;
+        }
+
+    }
+}
