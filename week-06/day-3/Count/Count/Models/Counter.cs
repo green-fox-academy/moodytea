3aa6bf4a09a3a2f370f532c5585b5bcab46a54f5
moodytea <moodytea@yahoo.com>
2017-10-26 13:49:21 +0200

    counter done

---
+++ week-06/day-3/Count/Count/Models/Counter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Count.Models
+{
+    public class Counter
+    {
+        public int Number { get; set; } = 1;
+
+        public void Raise()
+        {
+            Number++;
+        }
+    }
+}
--- week-06/day-3/Count/Count/Startup.cs
+++ week-06/day-3/Count/Count/Startup.cs
@@ -16,6 +16,8 @@ namespace Count
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddSingleton<Models.Counter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +29,8 @@ namespace Count
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvc();
 
             app.Run(async (context) =>
             {
