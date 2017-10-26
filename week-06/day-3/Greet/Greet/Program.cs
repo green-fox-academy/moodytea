6aba6bc22f773c06f0149fee10984761fd0eda73
moodytea <moodytea@yahoo.com>
2017-10-26 13:33:38 +0200

    created some projects for today

--- week-06/day-3/Greet/Greet/Program.cs
+++ week-06/day-3/Greet/Greet/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
-namespace TripsOfDoctorWho
+namespace Greet
 {
     public class Program
     {
--- week-06/day-3/Greet/Greet/Startup.cs
+++ week-06/day-3/Greet/Greet/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace TripsOfDoctorWho
+namespace Greet
 {
     public class Startup
     {
