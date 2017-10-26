ed069c45cda49b1bc7f3677c5e0bc23bfe0b075f
moodytea <moodytea@yahoo.com>
2017-10-26 17:13:01 +0200

    returns greeting

--- week-06/day-3/Greet/Greet/Startup.cs
+++ week-06/day-3/Greet/Greet/Startup.cs
@@ -16,6 +16,8 @@ namespace Greet
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddSingleton<Models.Greeting>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +29,8 @@ namespace Greet
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvc();
 
             app.Run(async (context) =>
             {
