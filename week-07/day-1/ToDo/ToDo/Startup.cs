a26f8cd122d41754557db0841779f9db7456e831
moodytea <moodytea@yahoo.com>
2017-10-31 14:15:39 +0100

    todo mostly created

--- week-07/day-1/ToDo/ToDo/Startup.cs
+++ week-07/day-1/ToDo/ToDo/Startup.cs
@@ -12,21 +12,24 @@ namespace ToDo
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddSingleton<Models.ToDo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
+            app.UseStaticFiles();
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvcWithDefaultRoute();
 
             app.Run(async (context) =>
             {
