df2904460d77bc94230736942070dd72a55aa137
moodytea <moodytea@yahoo.com>
2017-11-17 08:46:02 +0100

    contoso part 2

--- week-09/Contoso/Contoso/Controllers/HomeController.cs
+++ week-09/Contoso/Contoso/Controllers/HomeController.cs
@@ -14,36 +14,50 @@ namespace Contoso.Models
     [Route("Home")]
     public class HomeController : Controller
     {
+        [Route("")]
+        //[Route("IndexWithForm")]
+        [Route("Index")]
+        [Route("Index/{id}")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("IndexWithForm");
+        }
 
         // GET: /<controller>/
         [Route("")]
         [Route("Index")]
         [Route("Index/{id}")]
-        public IActionResult Index(int id)
+        [HttpPost]
+        public IActionResult Index(/*[FromQuery]*/Contact contact)
         {
+            if (ModelState.IsValid)
+            {
+                Customer customer = new Customer()
+                {
+                    Id = 2,
+                    CustomerName = "The Company"
+                };
+
+                HomeIndexViewModel vm = new HomeIndexViewModel()
+                {
+                    Contact = contact,
+                    Customer = customer
+                };
+
+                return View(vm);
+                //return Content("Hey there sweetie!");
+            }
             //Response.StatusCode = 404;
 
-            Contact contact = new Contact()
-            {
-                Id = id,
-                FirstName = "Dorothy",
-                LastName = "Katona"
-            };
+            //Contact contact = new Contact()
+            //{
+            //    Id = id,
+            //    FirstName = "Dorothy",
+            //    LastName = "Katona"
+            //};
 
-            Customer customer = new Customer()
-            {
-                Id = 2,
-                CustomerName = "The Company"
-            };
-
-            HomeIndexViewModel vm = new HomeIndexViewModel()
-            {
-                Contact = contact,
-                Customer = customer
-            };
-
-            return View(vm);
-            //return Content("Hey there sweetie!");
+            return View("IndexWithForm");
         }
     }
 }
---
+++ week-09/Contoso/Contoso/Models/ContosoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contoso.Models;
+
+namespace Contoso.Models
+{
+    public class ContosoContext : DbContext
+    {
+        public ContosoContext(DbContextOptions<ContosoContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
