8fb9a0bfcd422fa0dc32b31f33294d9a9065b652
moodytea <moodytea@yahoo.com>
2017-12-01 13:37:34 +0100

    bad-bad code but first iteration

--- week-42/TicTacToe/TicTacToe/Model/GameEngine.cs
+++ week-42/TicTacToe/TicTacToe/Model/GameEngine.cs
@@ -35,7 +35,76 @@ namespace TicTacToe.Model
 
                 if (ticTacToeBoard[positionX, positionY].Equals(" ")) ticTacToeBoard[positionX, positionY] = currentPlayer;
 
+                PrintTicTacToeBoard(ticTacToeBoard);
+
                 Winner = CheckForVictory(ticTacToeBoard, currentPlayer);
+
+                currentPlayer = currentPlayer.Equals(player1) ? player2 : player1;
+            }
+        }
+
+        private string CheckForVictory(string [,] ticTacToeBoard, string currentPlayer)
+        {
+            var HorizontalVictory = 0;
+            var VerticalVictory = 0;
+            var DiagonalVictory = 0;
+
+            for (int i = 0; i < ticTacToeBoard.GetLength(0); i++)
+            {
+                if (HorizontalVictory == 3)
+                    break;
+                else
+                    HorizontalVictory = 0;
+                for (int j = 0; j < ticTacToeBoard.GetLength(0); j++)
+                {
+                    if (ticTacToeBoard[i, j].Equals(currentPlayer))
+                        HorizontalVictory++;
+                }
+            }
+
+            for (int i = 0; i < ticTacToeBoard.GetLength(0); i++)
+            {
+                if (VerticalVictory == 3)
+                    break;
+                else
+                    VerticalVictory = 0;
+                for (int j = 0; j < ticTacToeBoard.GetLength(0); j++)
+                {
+                    if (ticTacToeBoard[j, i].Equals(currentPlayer))
+                        VerticalVictory++;
+                }
+            }
+
+            for (int i = 0; i < ticTacToeBoard.GetLength(0); i++)
+            {
+                if (ticTacToeBoard[i, i].Equals(currentPlayer))
+                    DiagonalVictory++;
+            }
+
+            if(DiagonalVictory != 3)
+            {
+                DiagonalVictory = 0;
+                for (int i = 0; i < ticTacToeBoard.GetLength(0); i++)
+                {
+                    if (ticTacToeBoard[(ticTacToeBoard.GetLength(0) - 1) - i, i].Equals(currentPlayer))
+                        DiagonalVictory++;
+                }
+                    
+            }
+
+            return HorizontalVictory == 3 || VerticalVictory == 3 || DiagonalVictory == 3 ? currentPlayer : string.Empty;
+        }
+
+        private void PrintTicTacToeBoard(string[,] ticTacToeBoard)
+        {
+            for (int i = 0; i < ticTacToeBoard.GetLength(0); i++)
+            {
+                for (int j = 0; j < ticTacToeBoard.GetLength(0); j++)
+                {
+                    Console.Write(ticTacToeBoard[i,j] + " ");
+                }
+
+                Console.WriteLine();
             }
         }
 
