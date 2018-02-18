62c2cb603f2fb3d1dfe471ec43dff5d71c3e9bd5
moodytea <moodytea@yahoo.com>
2018-02-18 19:26:00 +0100

    even simpler player moves created

---
+++ uni/PlayerMove.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+
+public class PlayerMove : MonoBehaviour {
+
+	public Rigidbody rigidBody;
+    public float forwardForce = 2000f;
+    public float sidewayForce = 500f;
+
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+	
+	void FixedUpdate () {
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidBody.AddForce(sidewayForce*Time.deltaTime, 0, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidBody.AddForce(-sidewayForce* Time.deltaTime, 0, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidBody.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidBody.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        }
+
+
+    }
+}
